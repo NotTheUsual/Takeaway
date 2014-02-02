9909e55a8b34846745c777f88d7d60b9c35f72c6
James Hunter <james.hunter@me.com>
2014-02-02 11:13:52 +0000

    Moved all the interface stuff to a module. I'm not sure any of this was a good idea

---
+++ lib/interface.rb
@@ -0,0 +1,70 @@
+module Interface
+	def peruse
+		puts "Welcome! What would you like to do?"
+		loop do
+			print_options
+			selection = gets.chomp
+			case selection
+			when "Menu"
+				print_menu
+			when "Order"
+				take_order
+			when "Exit"
+				exit
+			end
+		end
+	end
+
+	def print_options
+		puts "Menu".ljust(7)  + "| See all the items available for order"
+		puts "Order".ljust(7) + "| Place an order"
+		puts "Exit".ljust(7)  + "| Leave the takeaway"
+	end
+
+	def print_menu
+		puts "-" * 35
+		puts menu
+		puts "-" * 35
+	end
+
+	def take_order
+		order = Order.new
+		puts "What would you like to eat?"
+		puts "(type 'done' when done)"
+		dish_name = gets.chomp
+		while dish_name != 'done'
+			begin
+				dish = find_dish_by_name(dish_name)
+			rescue ArgumentError => e
+				puts e.message
+				ask_for_another_dish
+				dish_name = gets.chomp
+				next
+			end
+			puts "How many would you like?"
+			quantity = gets.chomp.to_i
+			order.add_line(LineItem.new(dish,quantity))
+			ask_for_another_dish
+			dish_name = gets.chomp
+		end
+		pay order
+	end
+
+	def ask_for_another_dish
+		puts "Would you like to select another dish?"
+		puts "(type 'done' when done)"
+	end
+
+	def pay_for order
+		puts "That'll be £#{order.total}"
+		payment = gets.chomp.to_f
+		begin
+			place_order(order, payment)
+		rescue
+			puts "I'm sorry, but that's not the right amount - please try again"
+			pay_for order
+		else
+			puts "Thank you for your order"
+		end
+	end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,21 @@
+require "takeaway"
+require "dish"
+require "order"
+require "line_item"
+require './lib/interface'
+
+RSpec.configure do |config|
+  # This is the thing that means it won't print out anything in the tests.
+  # I hope
+  original_stderr = $stderr
+  original_stdout = $stdout
+  config.before(:all) do 
+    # Redirect stderr and stdout
+    $stderr = File.new(File.join(File.dirname(__FILE__), '.', 'output.txt'), 'w')
+    $stdout = File.new(File.join(File.dirname(__FILE__), '.', 'output.txt'), 'w')
+  end
+  config.after(:all) do 
+    $stderr = original_stderr
+    $stdout = original_stdout
+  end
+end
