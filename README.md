86bf9ace3f5deb8e20b7c61bfc635366cccdc20e
James Hunter <james.hunter@me.com>
2014-02-02 14:56:58 +0000

    Updated README with instructions

--- README.md
+++ README.md
@@ -1,3 +1,70 @@
 # Takeaway
 
 Models a system for managing orders at takeaway restaurants.
+
+## Main Classes
+
+OK, so there are four main classes
+- Takeaway
+- Dish
+- Order
+- LineItem
+
+The Takeaway can
+- add Dishes
+- return a menu of dish names and prices
+- place an order, with a given Order and payment value
+- find a dish from the menu given its name
+- send a confirmation message (well, kinda...)
+
+Each Dish
+- has a name
+- has a price
+
+An Order
+- has a list of LineItems
+- can add a new LineItem
+- can generate its total value
+
+Each LineItem
+- has a dish
+- has a quantity
+- can work out its cost
+
+So that's how that works. There's a version with only the Takeaway and Dish classes tagged as Final-Takeaway-Dish-Version, but it was suggested that more classes might be a better approach, so now there are more classes. The Takeaway class can't actually send messages as Twilio have deleted my account and stopped responding to me, but all the stubs seem to work (and I have the text history to show it did at one point send (maybe too many) messages).
+
+## Interface
+
+Now, having spent a couple of weeks building things that don't actually work (well, I mean, they work, but you can't work them), I thought I'd try to graft the interface from the student directory onto this project, so I could work out how everything might link together. So, Takeaway now uses a module called Interface, which lets me play with it in irb. All the tests for this are commented out by default (both interface_spec.rb and final_test_spec.rb), as they aren't relevant to the specification and run slightly slowly owing to having to wait for Twilio to fail. This next bit is mostly for my reference later.
+
+The interface
+- can present a list of options
+- will call the Takeaway menu if requested
+- can take an order
+-- will ask for dishes (calling an error if the given dish is not found)
+-- will ask for a quantity
+-- will create a LineItem and add it to the order
+-- will repeat until 'done'
+- will ask for payment
+-- will repeat until the payment is correct
+- will apologise for Twilio failing
+
+To get it to work in irb
+		require './lib/takeaway'
+		require './lib/dish'
+		require './lib/order'
+		require './lib/line_item'
+
+		pizza_place	= Takeaway.new
+		cheese_tomato = Dish.new("Cheese and Tomato", 12.99)
+		new_york = Dish.new("New York Deli", 18.49)
+		carolina = Dish.new("The Carolina", 18.49)
+
+		pizza_place.add_dish(cheese_tomato)
+		pizza_place.add_dish(new_york)
+		pizza_place.add_dish(carolina)
+
+		pizza_place.peruse
+Then, follow onscreen instuctions (exactly - it won't check for spelling/case errors).
+
+It did help me conceptualise the exercise, but I'm not necessarily sure it produced any decent code... Still fun to be able to actually use something you've coded.
--- spec/interface_spec.rb
+++ spec/interface_spec.rb
@@ -1,251 +1,251 @@
-require 'stringio'
-require './spec/spec_helper'
+# require 'stringio'
+# require './spec/spec_helper'
 
-def capture_stdout &block
-  old_stdout = $stdout
-  fake_stdout = StringIO.new
-  $stdout = fake_stdout
-  block.call
-  fake_stdout.string
-ensure
-  $stdout = old_stdout
-end
+# def capture_stdout &block
+#   old_stdout = $stdout
+#   fake_stdout = StringIO.new
+#   $stdout = fake_stdout
+#   block.call
+#   fake_stdout.string
+# ensure
+#   $stdout = old_stdout
+# end
 
 
-class PizzaPlace; include Interface; end
+# class PizzaPlace; include Interface; end
 
-describe PizzaPlace do
-		let(:pizza_place)  { PizzaPlace.new }
-		let(:pizza)			 { double :dish, name: "Pizza", price: 4.99 }
-		let(:pizza_line)	 { double :lineitem, dish: pizza, quantity: 2 }
-		let(:pizza_order) { double :order, total: 9.98, lines: [pizza_line] }
+# describe PizzaPlace do
+# 		let(:pizza_place)  { PizzaPlace.new }
+# 		let(:pizza)			 { double :dish, name: "Pizza", price: 4.99 }
+# 		let(:pizza_line)	 { double :lineitem, dish: pizza, quantity: 2 }
+# 		let(:pizza_order) { double :order, total: 9.98, lines: [pizza_line] }
 
-	it "should be able to print options for a user" do
-		printed = capture_stdout do
-	    pizza_place.print_options
-	  end
-  	expect(printed).to eq("Menu   | See all the items available for order\nOrder  | Place an order\nExit   | Leave the takeaway\n")
-	end
+# 	it "should be able to print options for a user" do
+# 		printed = capture_stdout do
+# 	    pizza_place.print_options
+# 	  end
+#   	expect(printed).to eq("Menu   | See all the items available for order\nOrder  | Place an order\nExit   | Leave the takeaway\n")
+# 	end
 
-	context "(perusing)" do
-		before do
-			pizza_place.stub(:menu) { "Menu" }
-			pizza_place.stub(:take_order) { nil }
-		end
+# 	context "(perusing)" do
+# 		before do
+# 			pizza_place.stub(:menu) { "Menu" }
+# 			pizza_place.stub(:take_order) { nil }
+# 		end
 
-		it "should be able to exit the menu" do
-			allow(pizza_place).to receive(:gets).and_return("Exit\n")
+# 		it "should be able to exit the menu" do
+# 			allow(pizza_place).to receive(:gets).and_return("Exit\n")
 
-			expect{pizza_place.peruse}.to raise_error(SystemExit)
-		end
+# 			expect{pizza_place.peruse}.to raise_error(SystemExit)
+# 		end
 
-		it "should initially print a welcome message" do
-			allow(pizza_place).to receive(:gets).and_return("Exit\n")
+# 		it "should initially print a welcome message" do
+# 			allow(pizza_place).to receive(:gets).and_return("Exit\n")
 		 	
-		 	expect(pizza_place).to receive(:puts).with("Welcome! What would you like to do?")
-		 	expect(pizza_place).to receive(:puts).at_least(3).times
+# 		 	expect(pizza_place).to receive(:puts).with("Welcome! What would you like to do?")
+# 		 	expect(pizza_place).to receive(:puts).at_least(3).times
 		 	
-		 	expect{pizza_place.peruse}.to raise_error(SystemExit)
-		end
+# 		 	expect{pizza_place.peruse}.to raise_error(SystemExit)
+# 		end
 
-		it "should print the available options" do
-			allow(pizza_place).to receive(:gets).and_return("Exit\n")
+# 		it "should print the available options" do
+# 			allow(pizza_place).to receive(:gets).and_return("Exit\n")
 			
-			expect(pizza_place).to receive(:print_options)
-			expect{pizza_place.peruse}.to raise_error(SystemExit)
-		end
+# 			expect(pizza_place).to receive(:print_options)
+# 			expect{pizza_place.peruse}.to raise_error(SystemExit)
+# 		end
 
-		it "should get input from the user" do
-			allow(pizza_place).to receive(:gets).and_return("Exit\n")
+# 		it "should get input from the user" do
+# 			allow(pizza_place).to receive(:gets).and_return("Exit\n")
 			
-			expect(pizza_place).to receive(:gets)
-			expect{pizza_place.peruse}.to raise_error(SystemExit)
-		end
+# 			expect(pizza_place).to receive(:gets)
+# 			expect{pizza_place.peruse}.to raise_error(SystemExit)
+# 		end
 
-		it "should call the establishment's menu function when asked" do
-			allow(pizza_place).to receive(:gets).and_return("Menu\n", "Exit\n")
+# 		it "should call the establishment's menu function when asked" do
+# 			allow(pizza_place).to receive(:gets).and_return("Menu\n", "Exit\n")
 			
-			expect(pizza_place).to receive(:menu)
-			expect{pizza_place.peruse}.to raise_error(SystemExit)
-		end
+# 			expect(pizza_place).to receive(:menu)
+# 			expect{pizza_place.peruse}.to raise_error(SystemExit)
+# 		end
 
-		it "should print the menu when asked" do
-		  allow(pizza_place).to receive(:print_options).and_return(nil)
-		  allow(pizza_place).to receive(:gets).and_return("Menu\n", "Exit\n")
+# 		it "should print the menu when asked" do
+# 		  allow(pizza_place).to receive(:print_options).and_return(nil)
+# 		  allow(pizza_place).to receive(:gets).and_return("Menu\n", "Exit\n")
 
-		  expect(pizza_place).to receive(:puts).once
-		  expect(pizza_place).to receive(:puts).with('-' * 35)
-		  expect(pizza_place).to receive(:puts).with("Menu")
-		  expect(pizza_place).to receive(:puts).with('-' * 35)
+# 		  expect(pizza_place).to receive(:puts).once
+# 		  expect(pizza_place).to receive(:puts).with('-' * 35)
+# 		  expect(pizza_place).to receive(:puts).with("Menu")
+# 		  expect(pizza_place).to receive(:puts).with('-' * 35)
 
-		  expect{pizza_place.peruse}.to raise_error(SystemExit)
-		end
+# 		  expect{pizza_place.peruse}.to raise_error(SystemExit)
+# 		end
 
-		it "should take an order when asked" do
-			allow(pizza_place).to receive(:gets).and_return("Order\n", "Exit\n")
+# 		it "should take an order when asked" do
+# 			allow(pizza_place).to receive(:gets).and_return("Order\n", "Exit\n")
 			
-			expect(pizza_place).to receive(:take_order)
+# 			expect(pizza_place).to receive(:take_order)
 			
-			expect{pizza_place.peruse}.to raise_error(SystemExit)
-		end
+# 			expect{pizza_place.peruse}.to raise_error(SystemExit)
+# 		end
 
-		it "should be able to print the menu, then order" do
-			allow(pizza_place).to receive(:gets).and_return("Menu\n", "Order\n", "Exit\n")
+# 		it "should be able to print the menu, then order" do
+# 			allow(pizza_place).to receive(:gets).and_return("Menu\n", "Order\n", "Exit\n")
 			
-			expect(pizza_place).to receive(:menu)
-			expect(pizza_place).to receive(:take_order)
+# 			expect(pizza_place).to receive(:menu)
+# 			expect(pizza_place).to receive(:take_order)
 
-			expect{pizza_place.peruse}.to raise_error(SystemExit)
-		end
-	end
+# 			expect{pizza_place.peruse}.to raise_error(SystemExit)
+# 		end
+# 	end
 
-	context "(taking the order)" do
-		before do
-			allow(pizza_place).to receive(:find_dish_by_name).with("Pizza").and_return(pizza)
-			allow(pizza_place).to receive(:find_dish_by_name).with("Chips").and_raise(ArgumentError, "message")
-			allow(pizza_place).to receive(:pay_for).and_return(nil)
-		end
+# 	context "(taking the order)" do
+# 		before do
+# 			allow(pizza_place).to receive(:find_dish_by_name).with("Pizza").and_return(pizza)
+# 			allow(pizza_place).to receive(:find_dish_by_name).with("Chips").and_raise(ArgumentError, "message")
+# 			allow(pizza_place).to receive(:pay_for).and_return(nil)
+# 		end
 
-		it "should create a new order object" do
-			allow(pizza_place).to receive(:gets).and_return("done\n")
+# 		it "should create a new order object" do
+# 			allow(pizza_place).to receive(:gets).and_return("done\n")
 			
-			expect(Order).to receive(:new)
+# 			expect(Order).to receive(:new)
 			
-			pizza_place.take_order
-		end
+# 			pizza_place.take_order
+# 		end
 
-		it "should print out instructions" do
-			allow(pizza_place).to receive(:gets).and_return("done\n")
+# 		it "should print out instructions" do
+# 			allow(pizza_place).to receive(:gets).and_return("done\n")
 
-			expect(pizza_place).to receive(:puts).with("What would you like to eat?")
-			expect(pizza_place).to receive(:puts).with("(type 'done' when done)")
+# 			expect(pizza_place).to receive(:puts).with("What would you like to eat?")
+# 			expect(pizza_place).to receive(:puts).with("(type 'done' when done)")
 			
-			pizza_place.take_order
-		end
+# 			pizza_place.take_order
+# 		end
 
-		it "should take input form the user" do
-			allow(pizza_place).to receive(:gets).and_return("done\n")
+# 		it "should take input form the user" do
+# 			allow(pizza_place).to receive(:gets).and_return("done\n")
 			
-			expect(pizza_place).to receive(:gets)
-			pizza_place.take_order
-		end
+# 			expect(pizza_place).to receive(:gets)
+# 			pizza_place.take_order
+# 		end
 
-		it "should be able to break out of the loop after none-done input" do
-			allow(pizza_place).to receive(:gets).and_return("Pizza\n", "done\n")
+# 		it "should be able to break out of the loop after none-done input" do
+# 			allow(pizza_place).to receive(:gets).and_return("Pizza\n", "done\n")
 		
-			expect(pizza_place).to receive(:pay_for)
+# 			expect(pizza_place).to receive(:pay_for)
 
-			pizza_place.take_order
-		end
+# 			pizza_place.take_order
+# 		end
 
-		it "should search for the dish associated with the inputted name" do
-			allow(pizza_place).to receive(:gets).and_return("Pizza\n", "done\n")
+# 		it "should search for the dish associated with the inputted name" do
+# 			allow(pizza_place).to receive(:gets).and_return("Pizza\n", "done\n")
 		
-			expect(pizza_place).to receive(:find_dish_by_name).with("Pizza")
+# 			expect(pizza_place).to receive(:find_dish_by_name).with("Pizza")
 
-			pizza_place.take_order
-		end
+# 			pizza_place.take_order
+# 		end
 
-		it "should ask for a new input if the user asks for an off-menu dish" do
-			allow(pizza_place).to receive(:gets).and_return("Chips\n", "done\n")
+# 		it "should ask for a new input if the user asks for an off-menu dish" do
+# 			allow(pizza_place).to receive(:gets).and_return("Chips\n", "done\n")
 
-			expect(pizza_place).to receive(:puts).twice
-			expect(pizza_place).to receive(:puts).with("message")
-			expect(pizza_place).to receive(:puts).with("Would you like to select another dish?")
-			expect(pizza_place).to receive(:puts).with("(type 'done' when done)")
+# 			expect(pizza_place).to receive(:puts).twice
+# 			expect(pizza_place).to receive(:puts).with("message")
+# 			expect(pizza_place).to receive(:puts).with("Would you like to select another dish?")
+# 			expect(pizza_place).to receive(:puts).with("(type 'done' when done)")
 
-			pizza_place.take_order
-		end
+# 			pizza_place.take_order
+# 		end
 
-		it "should ask for a quantity once it has accepted a dish" do
-			allow(pizza_place).to receive(:gets).and_return("Pizza\n", "1\n", "done\n")
+# 		it "should ask for a quantity once it has accepted a dish" do
+# 			allow(pizza_place).to receive(:gets).and_return("Pizza\n", "1\n", "done\n")
 
-			expect(pizza_place).to receive(:puts).exactly(2).times
-			expect(pizza_place).to receive(:puts).with("How many would you like?")
-			expect(pizza_place).to receive(:gets)
-			expect(pizza_place).to receive(:puts).at_least(2).times
+# 			expect(pizza_place).to receive(:puts).exactly(2).times
+# 			expect(pizza_place).to receive(:puts).with("How many would you like?")
+# 			expect(pizza_place).to receive(:gets)
+# 			expect(pizza_place).to receive(:puts).at_least(2).times
 
-			pizza_place.take_order
-		end
+# 			pizza_place.take_order
+# 		end
 
-		it "should create a new line item with the dish and the quantity" do
-			allow(pizza_place).to receive(:gets).and_return("Pizza\n", "1\n", "done\n")
+# 		it "should create a new line item with the dish and the quantity" do
+# 			allow(pizza_place).to receive(:gets).and_return("Pizza\n", "1\n", "done\n")
 
-			expect(LineItem).to receive(:new).with(pizza,1)
+# 			expect(LineItem).to receive(:new).with(pizza,1)
 
-			pizza_place.take_order
-		end
+# 			pizza_place.take_order
+# 		end
 
-		it "should add the line item to the order" do
-			allow(pizza_place).to receive(:gets).and_return("Pizza\n", "1\n", "done\n")
+# 		it "should add the line item to the order" do
+# 			allow(pizza_place).to receive(:gets).and_return("Pizza\n", "1\n", "done\n")
 
-			expect_any_instance_of(Order).to receive(:add_line)
+# 			expect_any_instance_of(Order).to receive(:add_line)
 
-			pizza_place.take_order
-		end
+# 			pizza_place.take_order
+# 		end
 
-		it "should ask for a new dish once it has added the item to the order" do
-			allow(pizza_place).to receive(:gets).and_return("Pizza\n", "1\n", "done\n")
+# 		it "should ask for a new dish once it has added the item to the order" do
+# 			allow(pizza_place).to receive(:gets).and_return("Pizza\n", "1\n", "done\n")
 
-			expect(pizza_place).to receive(:puts).exactly(3).times
-			expect(pizza_place).to receive(:puts).with("Would you like to select another dish?")
-			expect(pizza_place).to receive(:puts).with("(type 'done' when done)")
+# 			expect(pizza_place).to receive(:puts).exactly(3).times
+# 			expect(pizza_place).to receive(:puts).with("Would you like to select another dish?")
+# 			expect(pizza_place).to receive(:puts).with("(type 'done' when done)")
 
-			pizza_place.take_order
-		end
-	end
+# 			pizza_place.take_order
+# 		end
+# 	end
 
-	context "(attempting to pay)" do
-		before do
-			allow(pizza_place).to receive(:place_order).with(pizza_order,9.98).and_return(nil)
-			allow(pizza_place).to receive(:place_order).with(pizza_order,8.88).and_raise(ArgumentError, "message")
-		end
+# 	context "(attempting to pay)" do
+# 		before do
+# 			allow(pizza_place).to receive(:place_order).with(pizza_order,9.98).and_return(nil)
+# 			allow(pizza_place).to receive(:place_order).with(pizza_order,8.88).and_raise(ArgumentError, "message")
+# 		end
 
-		it "should ask you to pay the correct total" do
-			allow(pizza_place).to receive(:gets).and_return("9.98")
+# 		it "should ask you to pay the correct total" do
+# 			allow(pizza_place).to receive(:gets).and_return("9.98")
 
-			expect(pizza_place).to receive(:puts).with("That'll be £9.98")
-			expect(pizza_place).to receive(:puts).at_least(1).times
+# 			expect(pizza_place).to receive(:puts).with("That'll be £9.98")
+# 			expect(pizza_place).to receive(:puts).at_least(1).times
 
-			pizza_place.pay_for(pizza_order)
-		end
+# 			pizza_place.pay_for(pizza_order)
+# 		end
 
-		it "should take payment from the user (as typed in number)" do
-			allow(pizza_place).to receive(:gets).and_return("9.98")
+# 		it "should take payment from the user (as typed in number)" do
+# 			allow(pizza_place).to receive(:gets).and_return("9.98")
 		
-			expect(pizza_place).to receive(:gets)
+# 			expect(pizza_place).to receive(:gets)
 
-			pizza_place.pay_for(pizza_order)
-		end
+# 			pizza_place.pay_for(pizza_order)
+# 		end
 
-		it "should call place_order with the computed order" do
-			allow(pizza_place).to receive(:gets).and_return("9.98")
+# 		it "should call place_order with the computed order" do
+# 			allow(pizza_place).to receive(:gets).and_return("9.98")
 
-			expect(pizza_place).to receive(:place_order).with(pizza_order,9.98)
+# 			expect(pizza_place).to receive(:place_order).with(pizza_order,9.98)
 		
-			pizza_place.pay_for(pizza_order)
-		end
+# 			pizza_place.pay_for(pizza_order)
+# 		end
 
-		it "should handle the error if you add the wrong payment" do
-			allow(pizza_place).to receive(:gets).and_return("8.88", "9.98")
+# 		it "should handle the error if you add the wrong payment" do
+# 			allow(pizza_place).to receive(:gets).and_return("8.88", "9.98")
 
-			expect(pizza_place).to receive(:puts).once
-			expect(pizza_place).to receive(:puts).with("I'm sorry, but that's not the right amount - please try again")
-			expect(pizza_place).to receive(:puts).at_least(2).times
+# 			expect(pizza_place).to receive(:puts).once
+# 			expect(pizza_place).to receive(:puts).with("I'm sorry, but that's not the right amount - please try again")
+# 			expect(pizza_place).to receive(:puts).at_least(2).times
 
-			expect{pizza_place.pay_for(pizza_order)}.not_to raise_error
-		end
+# 			expect{pizza_place.pay_for(pizza_order)}.not_to raise_error
+# 		end
 
-		# I wanted a test for the recursive call, but receive(:pay_for).once/twice didn't track right
-		# So, checking for the extra puts statement above does track, for now, but is a little brittle
+# 		# I wanted a test for the recursive call, but receive(:pay_for).once/twice didn't track right
+# 		# So, checking for the extra puts statement above does track, for now, but is a little brittle
 
-		it "should print a confirmation message before the end" do
-			allow(pizza_place).to receive(:gets).and_return("9.98")
+# 		it "should print a confirmation message before the end" do
+# 			allow(pizza_place).to receive(:gets).and_return("9.98")
 
-			expect(pizza_place).to receive(:puts).once
-			expect(pizza_place).to receive(:puts).with("Thank you for your order")
+# 			expect(pizza_place).to receive(:puts).once
+# 			expect(pizza_place).to receive(:puts).with("Thank you for your order")
 
-			pizza_place.pay_for(pizza_order)
-		end
-	end
-end
+# 			pizza_place.pay_for(pizza_order)
+# 		end
+# 	end
+# end
